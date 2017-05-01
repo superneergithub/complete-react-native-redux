4b751954b59d18a99419201b4c7403d2e2c9ac01
Israel Vicars <israelvicars@gmail.com>
2017-05-01 13:44:24 -0400

    Completed albums viewer React Native app

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -1,14 +1,13 @@
-// index.ios.js - place code in here for iOS
-
-// Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
-// Create a component
 const App = () => (
-  <Header headerText={'Albums'} />
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
-// Render it to the device
 AppRegistry.registerComponent('albums', () => App);
--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,8 +1,6 @@
-// import libraries for making a component
 import React from 'react';
 import { Text, View } from 'react-native';
 
-// make a component
 const Header = (props) => {
   const { viewStyle, textStyle } = styles;
 
@@ -31,5 +29,4 @@ const styles = {
   },
 };
 
-// make the component available to other parts of the app
 export default Header;
